848c782a32fa40fcb74995736d2ca6b27aef67bd
Francisco Murua Leoni <68504570+FranML12@users.noreply.github.com>
2024-09-08 15:30:25 -0300

    Se añade TP_03 (no finalizado)

--- TP_03/funcs.py
+++ TP_03/funcs.py
@@ -1,1 +1,205 @@
 # Archivo de funciones a importar a main
+def es_letra(c):
+    return 'A' <= c <= 'Z' or 'a' <= c <= 'z'
+
+
+def es_mayus(c):
+    return 'A' <= c <= 'Z'
+
+
+def es_digito(c):
+    return '0' <= c <= '9'
+
+
+def borrar_espacios(rango):
+    resultado = ''
+    for i in rango:
+        if i != ' ':
+            resultado += i
+    return resultado
+
+def borrar_puntos(rango):
+    resultado = ''
+    for i in rango:
+        if i != '.':
+            resultado += i
+    return resultado
+
+def verificador(n:int,lower:int,upper:int):
+    if lower <= n <= upper:
+        return n 
+    else:
+        return None
+
+
+def pais(cp):
+    if len(cp) == 9 and cp[5] == '-' and es_letra(cp[:5]) and es_letra(cp[6:]):
+        pais = 'Brasil'
+    elif len(cp) == 8 and es_letra(cp[0]) and es_letra(cp[5:]) and es_digito(cp[1:5]):
+        pais = 'Argentina'
+    elif es_digito(cp):
+        if len(cp) == 7:
+            pais = 'Chile'
+        elif len(cp) == 6:
+            pais = 'Paraguay'
+        elif len(cp) == 5:
+            pais = 'Uruguay'
+            if cp[0] == '1':
+                pais = 'Montevideo'
+        elif len(cp) == 4:
+            pais = 'Bolivia'
+        else:
+            pais = 'Otros países'
+    else:
+        pais = 'Otros países'
+        
+    return pais
+
+def final_amount(cp, destino, tipo, pago):
+    # determinación del importe inicial a pagar.
+    importes = (1100, 1800, 2450, 8300, 10900, 14300, 17900)
+    monto = importes[tipo]
+
+    if destino == 'Argentina':
+        inicial = monto
+    else:
+        if destino == 'Bolivia' or destino == 'Paraguay' or (destino == 'Uruguay' and cp[0] == '1'):
+            inicial = int(monto * 1.20)
+        elif destino == 'Chile' or (destino == 'Uruguay' and cp[0] != '1'):
+            inicial = int(monto * 1.25)
+        elif destino == 'Brasil':
+            if cp[0] == '8' or cp[0] == '9':
+                inicial = int(monto * 1.20)
+            else:
+                if cp[0] == '0' or cp[0] == '1' or cp[0] == '2' or cp[0] == '3':
+                    inicial = int(monto * 1.25)
+                else:
+                    inicial = int(monto * 1.30)
+        else:
+            inicial = int(monto * 1.50)
+
+    # determinación del valor final del ticket a pagar.
+    # asumimos que es pago en tarjeta...
+    final = inicial
+
+    # ... y si no lo fuese, la siguiente será cierta y cambiará el valor...
+    if pago == 1:
+        final = int(0.9 * inicial)
+
+    return final
+
+def check_dir(direccion):
+    
+    cl = cd = 0
+    td = False
+    ant = " "
+    for car in direccion:
+        if car in " .":
+            # fin de palabra...
+            # un flag si la palabra tenia todos sus caracteres digitos...
+            if cl == cd:
+                td = True
+
+            # resetear variables de uso parcial...
+            cl = cd = 0
+            ant = " "
+
+        else:
+            # en la panza de la palabra...
+            # contar la cantidad de caracteres de la palabra actual...
+            cl += 1
+
+            # si el caracter no es digito ni letra, la direccion no es valida... salir con False...
+            if not car.isdigit() and not car.isalpha():
+                return False
+
+            # si hay dos mayusculas seguidas, la direccion no es valida... salir con False...
+            if ant.isupper() and car.isupper():
+                return False
+
+            # contar digitos para saber si hay alguna palabra compuesta solo por digitos...
+            if car.isdigit():
+                cd += 1
+
+            ant = car
+
+    # si llegamos acá, es porque no había dos mayusculas seguidas y no habia caracteres raros...
+    # ... por lo tanto, habria que salir con True a menos que no hubiese una palabra con todos digitos...
+    return td
+
+""" def valida_para_hc(destino):
+    for i in range(len(destino)):
+        car = destino[i]
+        mayuscula_antes = False
+        hay_dos_may_consecutivas = False 
+        benditas_palabras = ""
+        palabras_y_digitos_pa = False
+
+        if not (('a'<= car <= 'z') or ('A'<= car <= 'Z') or ('0'<= car <= '9') or car == ' '):
+            return False
+        if ('A'<= car <= 'Z'):
+            if mayuscula_antes:
+                hay_dos_may_consecutivas = True
+                break
+            mayuscula_antes = True
+        else: 
+            mayuscula_antes = False
+        if car != ' ':
+            benditas_palabras += car
+        else: 
+            if len(benditas_palabras) > 0 and es_letra(benditas_palabras):
+                palabras_y_digitos_pa = True
+        benditas_palabras = ""
+    if len(benditas_palabras) > 0 and es_letra(benditas_palabras):
+        palabras_y_digitos_pa = True
+    if palabras_y_digitos_pa and not hay_dos_may_consecutivas:
+        return True
+    else:
+        return False """
+
+def valida_para_hc(envios):
+    # Esta lista almacenará los envíos con direcciones válidas
+    envios_validos = []
+
+    # Recorrer cada envío
+    for envio in envios:
+        destino = envio.destino
+        mayuscula_antes = False
+        hay_dos_may_consecutivas = False
+        tiene_palabra_numerica = False
+        palabra_actual = []
+        print(destino)
+        
+        for car in destino:
+            if car.isalpha() or car.isdigit() or car == ' ':
+                # Verificar si hay dos mayúsculas consecutivas
+                if car.isupper():
+                    if mayuscula_antes:
+                        hay_dos_may_consecutivas = True
+                        break
+                    mayuscula_antes = True
+                else:
+                    mayuscula_antes = False
+                
+                # Acumular caracteres de la palabra actual
+                if car != ' ':
+                    palabra_actual.append(car)
+                else:
+                    # Verificar si la palabra actual es numérica
+                    if len(palabra_actual) > 0 and all(c.isdigit() for c in palabra_actual):
+                        tiene_palabra_numerica = True
+                    palabra_actual = []
+            else:
+                # Si hay un carácter no válido, el envío no pasa la validación
+                hay_dos_may_consecutivas = True
+                break
+
+        # Verificar la última palabra acumulada
+        if len(palabra_actual) > 0 and all(c.isdigit() for c in palabra_actual):
+            tiene_palabra_numerica = True
+
+        # Validar si cumple todas las condiciones
+        if not hay_dos_may_consecutivas and tiene_palabra_numerica:
+            envios_validos.append(envio)
+
+    return envios_validos
--- TP_03/main.py
+++ TP_03/main.py
@@ -1,72 +1,46 @@
-""" 
-#primero eliminar espacios
-#segundo procesar del 0 al 8 (codigo postal)
-#tercero del 9 al 28 direccion fisica del destino (cadena de caracteres)
+from funcs import *
 
-# envios.txt -> proceso:
-# quitar espacios, definir tipo de envio, forma de pago, etc
-# Todo eso lo pusheo a un array "ENVIO"
-# [id=1, pais, tipo, cp, etc..]
-# envios [[id=1, pais, tipo, cp, etc..], 
-#         [id=2, pais, tipo, cp, etc..]]
+class Envio:
+    def __init__(self, cp: str, destino: str, tipo: int, pago: int):
+        self.codigo_postal = cp
+        self.destino = destino
+        self.tipo_envio = tipo
+        self.forma_pago = pago
+#inicializo la clase de envios y agregro los objetos respectivos y les asigno las variables de los futuros atributos
+    def __str__(self) -> str:
+        return f"{self.codigo_postal}, {self.destino}, {self.tipo_envio}, {self.forma_pago}"
+#escribo el __str__ para cuando se quiera printear o coso 
 
-# MENU :
-    1) cargar un arreglo con envios "enviostp3" (eliminando el viejo y si o 
-      si muestra un cartel de esta seguro ?)
-    2) cargar por teclado datos de envio(se guarda al final del array sin eliminar ninguno previo)
-    3) mostrar todos los array ordenados por codigo postal de menor a mayor
-    4) Buscar si existe en el arreglo un registro/objeto cuya dirección de 
-      envío sea igual a d y que sea del tipo de envío e, siendo d y e dos valores 
-      que se cargan por teclado
-    5) Buscar si existe en el arreglo un registro/objeto cuyo código 
-      postal sea igual a cp, siendo cp un valor que se carga por teclado
-"""
-"""if not os.path.exists(fd):
-        print('El archivo', fd, 'no existe...')
-        print('Revise, y reinicie el programa...')
-        exit(1)"""
-# primero determinar cantidad de datos
-# pasar los datos a las listas paralelas 
-# empezar con el menu
 
-data = open('envios-tp3.txt', 'rt')
-data_line = data.readline()
+def leer_archivo():
+    data = open('envios-tp3.txt', 'rt')
+    data_line = data.readline()
+    tipo_control = control(data_line)
+    print(tipo_control)    
 
-def main():
-    control_type = control(data_line)  # Procesa la primera línea
-    print(f"Tipo de control: {control_type}")
-
-    # Procesa el resto de las líneas
     envios = []
     while True:
         envio = arr(data)
         if envio:
             envios.append(envio)
-            # Puedes hacer más procesamiento aquí si es necesario
         else:
             break
-    print(envios)
-    
 
-def es_letra(c):
-    return 'A' <= c <= 'Z' or 'a' <= c <= 'z'
+    data.close()  # Cierra el archivo al terminar
+    return envios
 
 
-def es_mayus(c):
-    return 'A' <= c <= 'Z'
+def menu():
+    #creo una funcion menu donde printeo el menu y mando la opcion seleccionada del vuelta a la funcion principal(flores_maximo) para hacer lo que se tenga que hacer
+    menu = '1) cargar un arreglo con envios "enviostp3" (eliminando el viejo y si o si muestra un cartel de esta seguro ?) \n2) cargar por teclado datos de envio(se guarda al final del array sin eliminar ninguno previo)\n3) mostrar todos los array ordenados por codigo postal de menor a mayor\n4) Buscar si existe en el arreglo un registro/objeto cuya dirección de envío sea igual a d y que sea del tipo de envío e, siendo d y e dos valores que se cargan por teclado\n5) Buscar si existe en el arreglo un registro/objeto cuyo código postal sea igual a cp, siendo cp un valor que se carga por teclado\n0)Salir'
+    # usa \n apra bajar de linea
+    print(menu)
+    opcion = input('Introducir alguna opcion: ')
+    if not es_digito(opcion):
+        while not es_digito(opcion):
+            opcion = input('Introducir alguna opcion (Número): ')
+    return int(opcion)
 
-
-def es_digito(c):
-    return '0' <= c <= '9'
-
-
-def borrar_espacios(rango):
-    resultado = ''
-    for i in rango:
-        if i != ' ':
-            resultado += i
-            
-    return resultado
 
 def control(data_line):
     if 'HC' in data_line:
@@ -75,83 +49,239 @@ def control(data_line):
         return 'Soft Control'
     return 'Tipo de control desconocido'
 
+
 def arr(data):
-    data_line = data.readline()  # Lee la siguiente línea
+    data_line = data.readline()
     if data_line:
         cp = borrar_espacios(data_line[:9])  # Código postal
-        direccion = borrar_espacios(data_line[9:29])  # Dirección
-        tipo = borrar_espacios(data_line[29:30])  # Tipo de envío
-        pago = borrar_espacios(data_line[30:31])  # Método de pago
-        return [cp, direccion, tipo, pago]  # Devuelve una tupla con los datos
+        direccion = borrar_puntos(borrar_espacios(data_line[9:29]))  # Dirección
+        tipo = int(borrar_espacios(data_line[29:30]))  # Tipo de envío (convertido a int)
+        pago = int(borrar_espacios(data_line[30:31]))  # Método de pago (convertido a int)
+        return Envio(cp, direccion, tipo, pago)
     else:
-        data.close()
-        return None  # Si no hay más líneas, devuelve None
+        return None
 
-def pais(cp):
-    if len(cp) == 9 and cp[5] == '-' and es_letra(cp[:5]) and es_letra(cp[6:]):
-        pais = 'Brasil'
-    elif len(cp) == 8 and es_letra(cp[0]) and es_letra(cp[5:]) and es_digito(cp[1:5]):
-        pais = 'Argentina'
-    elif es_digito(cp):
-        if len(cp) == 7:
-            pais = 'Chile'
-        elif len(cp) == 6:
-            pais = 'Paraguay'
-        elif len(cp) == 5:
-            pais = 'Uruguay'
-            if cp[0] == '1':
-                pais = 'Montevideo'
-        elif len(cp) == 4:
-            pais = 'Bolivia'
+# FUNCION MAL PLANTEADA REVISAR!!!!
+
+# La opcion 1 es el postre (G. Golds) !IMPORTANT
+def guada_goldmorthi1(registro):
+    # registro = leer_archivo()# aqui se lee y se completa el registro
+    if registro != []:
+        respuesta = input("Hay registros cargados. ¿Desea eliminar los registros existentes y cargar de nuevo desde el archivo? (s/n): ")
+        if respuesta.lower() != 's':
+            print("Operación cancelada por el usuario.")
         else:
-            pais = 'Otros países'
+            print("Registros eliminados.")
+            registro = leer_archivo()# aqui se lee y se completa el registro
     else:
-        pais = 'Otros países'
-        
-    return pais
+        registro = leer_archivo()# aqui se lee y se completa el registro
+    return registro
 
-def precio_tipo(tipo):
-    if tipo == '0':
-        precio_tipo = 1100
-    elif tipo == '1':
-        precio_tipo = 1800
-    elif tipo == '2':
-        precio_tipo = 2450
-    elif tipo == '3':
-        precio_tipo = 8300
-    elif tipo == '4':
-        precio_tipo = 10900
-    elif tipo == '5':
-        precio_tipo = 14300
-    elif tipo == '6':
-        precio_tipo = 17900
-    else:
-        precio_tipo = 0  # Default value if tipo is not valid
-    
-    return precio_tipo
 
-def precio(pais, cp, precio):
-    veinte = ('Bolivia', 'Paraguay', 'Montevideo')
-    veintecinco = ('Chile', 'Uruguay')
-    
-    if pais == 'Brasil':
-        if cp[0] in '0123':
-            precio += precio * 0.25
-        elif cp[0] in '4567':
-            precio += precio * 0.3
+def guada_boiero2(registro):
+    # aca validamos si el registro existe o no, para cargar o no los datos que se piden cargar
+    cp = input("ingrese el codigo postal: ")
+    dirreccion = input('Ingrese la hermosisima dirrecion: ')
+    while True:
+            # cargamos datos de tipo de envio, si son validos devuelve el valor, sino devuelve un none
+        tipo_de_envio = verificador(int(input("Ingrese el tipo de envio del 0 al 6 \nCarta Simple: 0 (menos de 20 gramos), 1 (20gr <= peso < 150gr), 2 (150gr <= peso < 500gr)\nCarta Certificada: 3 (peso < 150gr), 4 (150gr <= peso < 500gr)\nCarta Expresa: 5 (peso < 150gr), 6 (150gr <= peso < 500gr)\nIngrese el tipo: ")),0,6)
+        if tipo_de_envio is None:
+            print("Tipo de envio invalido, ingresar nuevamente: ")
+            continue
         else:
-            precio += precio * 0.2
-    elif pais in veinte:
-        precio += precio * 0.2
-    elif pais in veintecinco:
-        precio += precio * 0.25
+            break
+    while True:
+        # cargamos la forma de pago, si es valida devuelve la forma (1 o 2), sino devuelve un none
+        forma_de_pago = verificador(int(input("ingrese la forma de pago del 1 al 2 (1(efectivo), 2(carton)): ")),1,2)
+        if forma_de_pago is None:
+            print("Tipo de envio invalido, ingresar nuevamente")
+            continue
+        else:
+            break
+    registro.append(Envio(cp ,dirreccion ,tipo_de_envio , forma_de_pago))
+
+
+def lucia_truco3(registro):
+    """
+    Muestra todos los registros del arreglo ordenados por código postal de menor a mayor.
+    Cada registro se muestra en una sola línea junto con el país al que pertenece el código postal.
+    Se puede elegir si mostrar todos los registros o solo los primeros m registros.
+    """
+    
+    n = len(registro)
+    
+    # Ordenamiento tipo Bubble Sort aplicado al arreglo de envíos
+    for i in range(n - 1):
+        ordenado = True
+        for j in range(n - i - 1):
+            if registro[j].codigo_postal > registro[j + 1].codigo_postal:
+                # Intercambia los objetos si el código postal es mayor
+                registro[j], registro[j + 1] = registro[j + 1], registro[j]
+                ordenado = False
+        if ordenado:
+            break  # Si ya está ordenado, termina el ciclo
+    
+    # Pregunta cuántos registros se deben mostrar
+    mostrar_todo = input("¿Mostrar todos los registros? (s/n): ").lower()
+    
+    if mostrar_todo == 'n':
+        m = int(input("¿Cuántos registros desea mostrar?: "))
     else:
-        precio += precio * 0.5
+        m = n  # Muestra todos si selecciona 's'
     
-    return precio
+    # Muestra los primeros m registros ordenados
+    for i in range(min(m, n)):
+        envio = registro[i]
+        pais_envio = pais(envio.codigo_postal)
+        print(f"{envio}, País: {pais_envio}")
 
-if __name__ == "__main__":
-    main()
+
+def I132qew44(registro):
+    d = input('Ingrese una dirrecion de envio (TODO JUNTO SIN ESPACIOS NI SIQUIERA ENTRE EL NOMBRE Y LOS NUMEROS): ')
+    e = int(input('Ingrese un tipo de envio: '))
+    # se ingresan los datos pedidos
+    for i in range(len(registro)):
+        if registro[i].destino.lower() == d.lower() and registro[i].tipo_envio == e: 
+            print(f'HENORABUENA, existe {registro[i]} y es mas, tecnicamente podriamos llegar a la conclucion de que, en efecto, se encuentra en el arreglo 🤓👆!!')
+            break
+        # con ayuda de la ingeniosa busqueda secuencial se 'busca' si aparece en el arreglo sino suelta un mensaje 
+    else:
+        print(f'No se encontro ese tipo {d} con el tipo de envio {e} en el arreglo')
+            
+
+def amerigo_jackson5(registro):#------------------>controlar si anda, teoricamente anda perfecto pero chequear
+    cp_buscar = input("Ingrese el código postal a buscar: ")
+    encontrado = False
+    izq = 0
+    der = len(Envio.codigo_postal)
+    # se asignan los valores de izq y derecha
+    while True:
+        mid = izq + der // 2
+        if Envio[mid].codigo_postal == cp_buscar:
+            print(Envio)
+            encontrado = True
+            if Envio[mid].forma_pago == 1:
+                Envio[mid].forma_pago = 2
+            elif Envio[mid].forma_pago == 2:
+                Envio[mid].forma_pago = 1 
+            # aqui se switchean las formas de pago de 1 a 2 y viceversa
+        if cp_buscar < Envio[mid].codigo_postal:
+            der -= mid # aqui vemos que si el valor del que buscamos es menor al del medio, se cambia el limite derecho
+        else:
+            izq += mid
+        break
+    if not encontrado:
+        print("No se encontró ningún registro con ese código postal.")
+
+
+""" def mateo_haro6(registro, tipo_control):
+    # Creamos un contador para cada tipo de envío
+    tipos_envio = [0] * 7  # Suponiendo que hay 7 tipos de envío (0 a 6)
+
+    if tipo_control == "Hard Control":
+        registro = obtener_registros_validos(registro)
     
+    # Recorremos el registro de envíos y contamos cada tipo
+    for envio in registro:
+        tipo_envio = envio.tipo_envio  # Accedemos al atributo 'tipo' correctamente con notación de puntos
+        if 0 <= tipo_envio < len(tipos_envio):
+            tipos_envio[tipo_envio] += 1
+    
+    # Mostramos los resultados
+    for i in range(len(tipos_envio)):
+        print(f"Tipo de envío {i}: {tipos_envio[i]} envíos")
+ """
+
+def mateo_haro6(registro, tipo_control):
+    tipos_envio = [0] * 7  # Conteo para cada tipo de envío
+    
+    if tipo_control == "Hard Control":
+        registro =valida_para_hc(registro)
+        print(registro)
+    
+    for envio in registro:
+        if 0 <= envio.tipo_envio < len(tipos_envio):
+            tipos_envio[envio.tipo_envio] += 1
+    
+    for i, count in enumerate(tipos_envio):
+        print(f"Tipo de envío {i}: {count} envíos")
 
 
+def luz_balastegui7(registro, tipo_control, envios_validos_hc=None):
+    """
+    Determina el importe final acumulado de cada uno de los siete tipos de envío.
+    
+    Si el tipo de control es 'Hard Control', solo considera los envíos con direcciones válidas.
+    Si el tipo de control es 'Soft Control', considera todos los envíos, independientemente de la validez de la dirección.
+    
+    registro: lista de envíos.
+    tipo_control: string que indica si es 'Hard Control' o 'Soft Control'.
+    envios_validos_hc: lista de envíos con direcciones válidas en caso de 'Hard Control'.
+    """
+    # Creamos un acumulador para cada tipo de envío
+    acumuladores = [0] * 7  # Un vector de acumuladores para los tipos de envío del 0 al 6
+
+    if tipo_control == "Hard Control" and envios_validos_hc is not None:
+        # Acumulamos solo los envíos con direcciones válidas
+        for envio in envios_validos_hc:
+            acumuladores[envio.tipo_envio] += envio.forma_pago
+
+    elif tipo_control == "Soft Control":
+        # Acumulamos todos los envíos, sin importar la validez de la dirección
+        for envio in registro:
+            acumuladores[envio.tipo_envio] += envio.forma_pago
+
+    # Mostrar el resultado de los acumuladores
+    for i in range(7):
+        print(f"Acumulador del tipo de envío {i}: {acumuladores[i]} pagos acumulados")
+
+
+def flores_maximo():
+    registro = []
+    tipo_control = None  # Inicializamos una variable para almacenar el tipo de control
+    op = None
+
+    while op != 0:
+        op = menu()      
+        if op == 1:
+            registro = guada_goldmorthi1(registro)
+            if registro:
+                # Determina el tipo de control tras cargar el archivo
+                d = open('envios-tp3.txt', 'rt')
+                d_line = d.readline()
+                tipo_control = control(d_line)
+                d.close()
+            print(tipo_control)
+        elif op == 2:
+            guada_boiero2(registro)         
+        elif str(op) in '3456789':
+            if registro == []:
+                print("El registro se encuentra vacío y no se pueden realizar búsquedas, bobi !.")
+            else:
+                if op == 3:
+                    lucia_truco3(registro)
+                elif op == 4:
+                    I132qew44(registro)
+                elif op == 5:
+                    amerigo_jackson5(registro)
+                elif op == 6:
+                    if tipo_control:  # Solo llamamos a la función si se ha definido el tipo de control
+                        mateo_haro6(registro, tipo_control)
+                    else:
+                        print("No se ha definido el tipo de control.")
+                elif op == 7:
+                    luz_balastegui7()
+                elif op == 8:
+                    pass
+                elif op == 9:
+                    pass
+        else:
+            print('\nFinalizado con éxito')
+
+
+
+if __name__ == '__main__':
+    flores_maximo()
+
+# TODO EMPEZO EN CRIMEA Y TERMINO EN CRIMEA!!! 
+# NO MAS A LA GUERRA PORFAVOR!
